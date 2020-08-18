1d16632c659b8d93e6c06770cc622f47389b3010
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-18 09:06:33 +0800

    注释无用模板代码

--- app/src/main/java/com/aimerneige/huawu/jungang/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/profile/ProfileViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
-    }
-    val text: LiveData<String> = _text
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is profile Fragment"
+//    }
+//    val text: LiveData<String> = _text
 }
