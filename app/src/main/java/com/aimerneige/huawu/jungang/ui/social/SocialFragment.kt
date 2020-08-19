d4cb9f2de2143fd6a17909993f369be4a70ac1a2
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-20 05:38:44 +0800

    注释无用模板代码

--- app/src/main/java/com/aimerneige/huawu/jungang/ui/social/SocialFragment.kt
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/social/SocialFragment.kt
@@ -1,14 +1,19 @@
 package com.aimerneige.huawu.jungang.ui.social
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.aimerneige.huawu.jungang.R
+import com.aimerneige.huawu.jungang.ui.MainActivity
+import com.allen.library.CircleImageView
+import kotlinx.android.synthetic.main.fragment_social.*
 
 class SocialFragment : Fragment() {
 
@@ -22,10 +27,11 @@ class SocialFragment : Fragment() {
         socialViewModel =
                 ViewModelProviders.of(this).get(SocialViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_social, container, false)
-        val textView: TextView = root.findViewById(R.id.text_social)
-        socialViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_social)
+//        socialViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
         return root
     }
 }
--- app/src/main/java/com/aimerneige/huawu/jungang/ui/social/SocialViewModel.kt
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/social/SocialViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class SocialViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is social Fragment"
-    }
-    val text: LiveData<String> = _text
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is social Fragment"
+//    }
+//    val text: LiveData<String> = _text
 }
