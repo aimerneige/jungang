c2e1c1ae326a3667b9c45079afd6fac730b2905c
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-20 07:09:23 +0800

    注释无用模板代码

--- app/src/main/java/com/aimerneige/huawu/jungang/ui/knowledge/KnowledgeFragment.kt
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/knowledge/KnowledgeFragment.kt
@@ -22,10 +22,10 @@ class KnowledgeFragment : Fragment() {
         knowledgeViewModel =
                 ViewModelProviders.of(this).get(KnowledgeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_knowledge, container, false)
-        val textView: TextView = root.findViewById(R.id.text_knowledge)
-        knowledgeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_knowledge)
+//        knowledgeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
--- app/src/main/java/com/aimerneige/huawu/jungang/ui/knowledge/KnowledgeViewModel.kt
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/knowledge/KnowledgeViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class KnowledgeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is knowledge Fragment"
-    }
-    val text: LiveData<String> = _text
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is knowledge Fragment"
+//    }
+//    val text: LiveData<String> = _text
 }
