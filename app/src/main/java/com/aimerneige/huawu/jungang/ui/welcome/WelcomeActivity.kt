46b0d56608dc526f126f2f3745f90f25765d138f
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-22 14:42:06 +0800

    welcome

---
+++ app/src/main/java/com/aimerneige/huawu/jungang/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,16 @@
+package com.aimerneige.huawu.jungang.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aimerneige.huawu.jungang.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        // TODO: Welcome View
+        //   TODO: Welcome Pages
+        //   TODO: Login Page
+    }
+}
